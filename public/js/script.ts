0d90bc1dec995c8d372f9daa8ff29c75caf7ef4e
maring0019 <maring0019@gmail.com>
2026-02-24 15:54:40 -0300

    esperando carga completa

--- public/js/script.ts
+++ public/js/script.ts
@@ -1,5 +1,5 @@
 /* Para redimensionar márgenes de nombre */
-
+console.log("carga archivo js");
 interface Lineas {
   lineHeightH: number;
   heightH: number;
@@ -58,29 +58,31 @@ function margin(
   console.log("contNombre.style.marginTop", contNombre.style.marginTop);
 }
 
-document.querySelectorAll(".container").forEach(container => {
+window.addEventListener("load", function () {
+  document.querySelectorAll(".container").forEach(container => {
 
-  const h2 = container.querySelector("h2") as HTMLElement | null;
-  console.log("h2", h2);
-  const h3 = container.querySelector("h3") as HTMLElement | null;
-  console.log("h3", h3);
-  const contNombre = document.querySelector(".contNombre") as HTMLElement | null;
-  console.log("contNombre", contNombre);
-  if (h2 && h3 && contNombre) {
-    console.log("entra al if, h2 h3 contNombre");
-    const lineasNombre = lineas(h2);
-    console.log("lineasNombre", lineasNombre);
-    console.log("lineasNombre.lineHeightH", lineasNombre.lineHeightH);
-    console.log("lineasNombre.heightH", lineasNombre.heightH);
-    console.log("lineasNombre.linesH", lineasNombre.linesH);
-    
-    const lineasFuncion = lineas(h3);
-    console.log("lineasFuncion", lineasFuncion);
-    console.log("lineasFuncion.lineHeightH", lineasFuncion.lineHeightH);
-    console.log("lineasFuncion.heightH", lineasFuncion.heightH);
-    console.log("lineasFuncion.linesH", lineasFuncion.linesH);
+    const h2 = container.querySelector("h2") as HTMLElement | null;
+    console.log("h2", h2);
+    const h3 = container.querySelector("h3") as HTMLElement | null;
+    console.log("h3", h3);
+    const contNombre = document.querySelector(".contNombre") as HTMLElement | null;
+    console.log("contNombre", contNombre);
+    if (h2 && h3 && contNombre) {
+      console.log("entra al if, h2 h3 contNombre");
+      const lineasNombre = lineas(h2);
+      console.log("lineasNombre", lineasNombre);
+      console.log("lineasNombre.lineHeightH", lineasNombre.lineHeightH);
+      console.log("lineasNombre.heightH", lineasNombre.heightH);
+      console.log("lineasNombre.linesH", lineasNombre.linesH);
+      
+      const lineasFuncion = lineas(h3);
+      console.log("lineasFuncion", lineasFuncion);
+      console.log("lineasFuncion.lineHeightH", lineasFuncion.lineHeightH);
+      console.log("lineasFuncion.heightH", lineasFuncion.heightH);
+      console.log("lineasFuncion.linesH", lineasFuncion.linesH);
 
-    margin(lineasFuncion, lineasNombre, contNombre);
-  }
+      margin(lineasFuncion, lineasNombre, contNombre);
+    }
 
+  });
 });
