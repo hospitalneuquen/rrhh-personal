df74063c3f390e697e7071bafa48dea1c7500c96
maring0019 <maring0019@gmail.com>
2026-02-24 13:11:01 -0300

    AJustando footer img y borderTop nombre.

--- public/js/script.ts
+++ public/js/script.ts
@@ -24,6 +24,8 @@ function margin(
   contNombre: HTMLElement
 ): void {
 
+  console.log("lineasFuncion.linesH", lineasFuncion.linesH);
+  console.log("lineasNombre.linesH", lineasNombre.linesH);
   switch (lineasFuncion.linesH) {
 
     case 1:
@@ -53,19 +55,30 @@ function margin(
       }
       break;
   }
+  console.log("contNombre.style.marginTop", contNombre.style.marginTop);
 }
-
 
 document.querySelectorAll(".container").forEach(container => {
 
   const h2 = container.querySelector("h2") as HTMLElement | null;
+  console.log("h2", h2);
   const h3 = container.querySelector("h3") as HTMLElement | null;
+  console.log("h3", h3);
   const contNombre = document.querySelector(".contNombre") as HTMLElement | null;
-
+  console.log("contNombre", contNombre);
   if (h2 && h3 && contNombre) {
-
+    console.log("entra al if, h2 h3 contNombre");
     const lineasNombre = lineas(h2);
+    console.log("lineasNombre", lineasNombre);
+    console.log("lineasNombre.lineHeightH", lineasNombre.lineHeightH);
+    console.log("lineasNombre.heightH", lineasNombre.heightH);
+    console.log("lineasNombre.linesH", lineasNombre.linesH);
+    
     const lineasFuncion = lineas(h3);
+    console.log("lineasFuncion", lineasFuncion);
+    console.log("lineasFuncion.lineHeightH", lineasFuncion.lineHeightH);
+    console.log("lineasFuncion.heightH", lineasFuncion.heightH);
+    console.log("lineasFuncion.linesH", lineasFuncion.linesH);
 
     margin(lineasFuncion, lineasNombre, contNombre);
   }
